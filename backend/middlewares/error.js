7acf136e881ab5bdedcfbf735190809904966d4a
surafelEmu <surafel097@outlook.com>
2021-10-06 20:31:24 +0300

    finising up error handling

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -18,7 +18,18 @@ exports.handleErrors = (err , req ,res , next) => {
     if(process.env.NODE_ENV == 'PRODUCTION') {
         let error = {...err}
 
-        error.message = err.message
+        error.message = err.message ;
+        if(err.name == 'CastError') {
+            const message =  `Resource not found. Invalid: ${err.path}`
+            error = new ErrorHandler(message , 400)
+        }
+
+        if(err.name == 'ValidationError') {
+            const message = Object.values(err.errors).map(value => value.message) ;
+            error = new ErrorHandler(message , 400)
+        }
+
+        // wrong mongoose object Id error
         res.status(error.statusCode).json({
             success: false ,
             message: error.message || 'Internal Server Error'
