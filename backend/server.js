a11dc0bd048c7016709ce33e20f6a61766ef8653
surafelEmu <surafel097@outlook.com>
2021-10-06 12:59:25 +0300

    configure mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const app = require('./index.js') ;
-
+const connectDB = require('./config/database.js'); 
 const dotenv = require('dotenv') ;
 
 dotenv.config({path: 'backend/config/config.env'}) ;
+
+connectDB() ;
 
 app.listen(process.env.PORT , () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} node.`) ;
