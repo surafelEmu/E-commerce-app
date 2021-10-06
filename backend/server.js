20d06cb595a8dc934893311a487ac21ef5489e23
surafelEmu <surafel097@outlook.com>
2021-10-06 20:13:45 +0300

    handling uncought and unhandledRejection exceptions

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,25 @@ const app = require('./index.js') ;
 const connectDB = require('./config/database.js'); 
 const dotenv = require('dotenv') ;
 
+process.on('uncaughtException' , err => {
+    console.log(`ERROR ${err.stack}`) ;
+    console.log('Shutting down due to uncought exception') ;
+    process.exit(1) ;
+})
+
 dotenv.config({path: 'backend/config/config.env'}) ;
 
 connectDB() ;
 
-app.listen(process.env.PORT , () => {
+const server = app.listen(process.env.PORT , () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} node.`) ;
+}) ;
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection' , err => {
+    console.log(`ERROR: ${err.message}`  ) ;
+    console.log('shutting down the server due to unhandled promise rejcetion') ;
+    server.close( () => {
+        process.exit(1) ;
+    })
 })
