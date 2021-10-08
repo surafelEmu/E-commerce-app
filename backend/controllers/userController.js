5a06fc49bc94d0132b45c4986e4232ee4c4dd7b0
surafelEmu <surafel097@outlook.com>
2021-10-08 14:21:24 +0300

    trying to send reset password reset email but some thing wrong with mail trap will be resolved later

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/userModel') ;
 const catchAsync = require('../middlewares/catchAsync') ;
 const ErrorHandler = require('../utils/errorHandler');
 const sendToken = require('../utils/jwtToken') ;
-
+const Email = require('../utils/sendEmail.js') ;
 
 exports.createUser = catchAsync(async (req , res , next) => {
 
@@ -56,4 +56,81 @@ exports.logout = catchAsync( async (req , res , next) => {
         success: true ,
         message: 'Logged out'
     })
-})
+}) ;
+
+
+// exports.forgotPassword = catchAsync(async (req , res , next) => {
+
+//     const user = await User.findOne({email: req.body.email}) ;
+
+//     if(!user) {
+//         return next(new ErrorHandler('user not found with email', 404)) ;
+//     }
+
+//     const resetToken = user.getResetPasswordToken() ;
+
+//     await user.save({validateBeforSave: false}) ;
+
+//     const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}` ;
+
+//     const message = `Your password reset token is as follow:\n\n${resetUrl}/n/n if you have not requested this email , then ignore it` ;
+
+//     try{
+
+//         await sendEmail({
+//             email: user.email ,
+//             subject: 'shopShop Password Recovery' ,
+//             message: message
+//         })
+
+//         res.status(200).json({
+//             success: true ,
+//             message: `Email sent to: ${user.email}`
+//         })
+
+//     } catch(error) {
+//         user.resetPasswordToken = undefined ;
+//         user.resetPasswordExpires = undefined ;
+
+//         await user.save({validateBeforSave: false}) ;
+
+//         return next(new ErrorHandler(error.message , 500))
+//     }
+
+// })
+
+
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+    // 1) Get user based on POSTed email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return next(new ErrorHandler('There is no user with email address.', 404));
+    }
+  
+    // 2) Generate the random reset token
+    const resetToken = user.getResetPasswordToken();
+    await user.save({ validateBeforeSave: false });
+  
+    // 3) Send it to user's email
+    try {
+      const resetURL = `${req.protocol}://${req.get(
+        'host'
+      )}/api/v1/users/resetPassword/${resetToken}`;
+      await new Email(user, resetURL).sendPasswordReset();
+  
+      res.status(200).json({
+        status: 'success',
+        message: 'Token sent to email!'
+      });
+    } catch (err) {
+      user.passwordResetToken = undefined;
+      user.passwordResetExpires = undefined;
+      await user.save({ validateBeforeSave: false });
+  
+      return next(
+        new ErrorHandler('There was an error sending the email. Try again later!'),
+        500
+      );
+    }
+  });
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -5,4 +5,5 @@ const router = express.Router() ;
 router.route('/register').post(userController.createUser) ;
 router.route('/signin').post(userController.loginUser) ;
 router.route('/logout').get(userController.logout) ;
+router.route('/password/forget').post(userController.forgotPassword) ;
 module.exports = router ;
