91d8d442ff350075b008d1365b6bbe340e79321c
surafelEmu <surafel097@outlook.com>
2021-10-11 15:00:17 +0300

    front end created with header and footer

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/layout/Header' ;
+import Footer from './components/layout/footer'
+
+import './App.css'
+function App() { 
+  return (
+    <div className="App">
+     <Header />
+     <Footer /> 
+    </div>
+  );
+}
+
+export default App;
