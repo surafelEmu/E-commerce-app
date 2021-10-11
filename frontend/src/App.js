32f2e56c43bfa65ae43f582bb4722a1405065702
surafelEmu <surafel097@outlook.com>
2021-10-11 16:00:13 +0300

    created ahome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import Header from './components/layout/Header' ;
 import Footer from './components/layout/footer'
+import Home from './components/Home'
+
+import { BrowserRouter as Router , Route} from 'react-router-dom' ;
 
 import './App.css'
 function App() { 
   return (
+    <Router>
     <div className="App">
      <Header />
+     <div className="container container-fluid">
+          <Route path = "/" component={Home} exact />
+     </div>
      <Footer /> 
     </div>
+    </Router>
+   
   );
 }
 
